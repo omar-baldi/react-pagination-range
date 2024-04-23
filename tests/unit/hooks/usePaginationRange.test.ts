d36d1d62efc275d7bc064653442bdfa6ac61255b
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-23 12:47:36 +0200

    Added tests to assert correct bahaviour when incrementing the page by 1

--- tests/unit/hooks/usePaginationRange.test.ts
+++ tests/unit/hooks/usePaginationRange.test.ts
@@ -1,5 +1,5 @@
 import { usePaginationRange } from "@/hooks/usePaginationRange";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 
 describe("usePaginationRange", () => {
   it("should custom hook be initialized with default values", () => {
@@ -15,5 +15,39 @@ describe("usePaginationRange", () => {
 
     expect(activePage).toBe(6);
     expect(range).toEqual([1, "...", 5, 6, 7, "...", 20]);
+  });
+
+  describe("When incrementing the page", () => {
+    it("should update active page and range if page is within range", () => {
+      const { result } = renderHook(() =>
+        usePaginationRange({
+          totalAmountElements: 20,
+          initialPage: 10,
+          siblingCount: 1,
+        })
+      );
+
+      expect(result.current.activePage).toBe(10);
+      expect(result.current.range).toEqual([1, "...", 9, 10, 11, "...", 20]);
+      act(() => result.current.incrementPage());
+      expect(result.current.activePage).toBe(11);
+      expect(result.current.range).toEqual([1, "...", 10, 11, 12, "...", 20]);
+    });
+
+    it("should not update active page and range when being at the last page", () => {
+      const { result } = renderHook(() =>
+        usePaginationRange({
+          totalAmountElements: 20,
+          initialPage: 20,
+          siblingCount: 1,
+        })
+      );
+
+      expect(result.current.activePage).toBe(20);
+      expect(result.current.range).toEqual([1, "...", 16, 17, 18, 19, 20]);
+      act(() => result.current.incrementPage());
+      expect(result.current.activePage).toBe(20);
+      expect(result.current.range).toEqual([1, "...", 16, 17, 18, 19, 20]);
+    });
   });
 });
