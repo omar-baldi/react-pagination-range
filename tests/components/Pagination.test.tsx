5e49089775b4cb4d180a81c99442a890aa75a4a8
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-25 18:57:49 +0200

    Added tests to assert correct behaviours when navigating to first and last page

--- tests/components/Pagination.test.tsx
+++ tests/components/Pagination.test.tsx
@@ -1,10 +1,19 @@
 /* eslint-disable */
 import Pagination from "@/components/Pagination";
+import { FIRST_PAGE } from "@/constants";
 import "@testing-library/jest-dom";
 import { fireEvent, render, RenderResult } from "@testing-library/react";
 
 function extractPagesButtonElements(wrapper: RenderResult, pagesArr: number[]) {
   return pagesArr.map((page) => wrapper.queryByTestId(`button-page button-page-${page}`));
+}
+
+function simulatePageClick(wrapper: RenderResult, page: number) {
+  const firstPageButton = wrapper.getByTestId(`button-page-${page}`, {
+    exact: false,
+  });
+
+  fireEvent.click(firstPageButton);
 }
 
 describe("Pagination", () => {
@@ -31,5 +40,58 @@ describe("Pagination", () => {
     const dotsElements = wrapper.queryAllByTestId("dots");
     buttonPages.forEach((buttonPage) => expect(buttonPage).not.toBeNull());
     expect(dotsElements.length).toBe(2);
+  });
+
+  describe("When clicking on the first page button", () => {
+    let wrapper: RenderResult;
+
+    beforeEach(() => {
+      wrapper = render(
+        <Pagination totalAmountElements={20} siblingCount={1} initialPage={6} />
+      );
+
+      simulatePageClick(wrapper, FIRST_PAGE);
+    });
+
+    it("should update elements rendered in the DOM", () => {
+      const buttonPages = extractPagesButtonElements(wrapper, [1, 2, 3, 4, 5, 20]);
+      const dotsElements = wrapper.queryAllByTestId("dots");
+      buttonPages.forEach((buttonPage) => expect(buttonPage).not.toBeNull());
+      expect(dotsElements.length).toBe(1);
+    });
+
+    it("should button be disabled", () => {
+      const prevPageButton = wrapper.getByTestId("prev-page-button");
+      expect(prevPageButton).toBeDisabled();
+    });
+  });
+
+  describe("When clicking on the last page button", () => {
+    let wrapper: RenderResult;
+
+    beforeEach(() => {
+      const totalAmountElements = 20;
+      wrapper = render(
+        <Pagination
+          totalAmountElements={totalAmountElements}
+          siblingCount={1}
+          initialPage={6}
+        />
+      );
+
+      simulatePageClick(wrapper, totalAmountElements);
+    });
+
+    it("should update elements rendered in the DOM", () => {
+      const buttonPages = extractPagesButtonElements(wrapper, [1, 16, 17, 18, 19, 20]);
+      const dotsElements = wrapper.queryAllByTestId("dots");
+      buttonPages.forEach((buttonPage) => expect(buttonPage).not.toBeNull());
+      expect(dotsElements.length).toBe(1);
+    });
+
+    it("should button be disabled", () => {
+      const nextPageButton = wrapper.getByTestId("next-page-button");
+      expect(nextPageButton).toBeDisabled();
+    });
   });
 });
